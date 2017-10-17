1bfe1b5dc8ba818a3eefbee894a7e1808b44f325
the-Infected-Drake <32571055+the-Infected-Drake@users.noreply.github.com>
2017-10-17 16:34:58 +0530

    Update googleSearch.py

--- tidos/googleSearch.py
+++ tidos/googleSearch.py
@@ -10,6 +10,7 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
    ENDC = '\033[0m'
+   BOLD = '\033[1m'
 W  = '\033[0m'  # white (normal)
 R  = '\033[31m' # red
 G  = '\033[32m' # green
@@ -25,21 +26,21 @@ def googleSearch():
 		sys.exit(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "             You are not using a Linux Based OS! Linux is a must-have for this script!" + color.END)
 	print ''
 	print ''
-	print ""+B+"                                 _____   _____ _____  ___  ______  _____  _   _ "
+	print ""+B+color.BOLD+"                                 _____   _____ _____  ___  ______  _____  _   _ "
 	time.sleep(0.3)
-	print ""+B+"                                |  __ \ /  ___|  ___|/ _ \ | ___ \/  __ \| | | |"
+	print ""+B+color.BOLD+"                                |  __ \ /  ___|  ___|/ _ \ | ___ \/  __ \| | | |"
 	time.sleep(0.3)
-	print ""+B+"                                | |  \/ \ `--.| |__ / /_\ \| |_/ /| /  \/| |_| |"
+	print ""+B+color.BOLD+"                                | |  \/ \ `--.| |__ / /_\ \| |_/ /| /  \/| |_| |"
 	time.sleep(0.3)
-	print ""+B+"                                | | __   `--. \  __||  _  ||    / | |    |  _  |"
+	print ""+B+color.BOLD+"                                | | __   `--. \  __||  _  ||    / | |    |  _  |"
 	time.sleep(0.3)
-	print ""+B+"                                | |_\ \ /\__/ / |___| | | || |\ \ | \__/\| | | |"
+	print ""+B+color.BOLD+"                                | |_\ \ /\__/ / |___| | | || |\ \ | \__/\| | | |"
 	time.sleep(0.3)
-	print ""+B+"                                 \____/ \____/\____/\_| |_/\_| \_| \____/\_| |_/"
+	print ""+B+color.BOLD+"                                 \____/ \____/\____/\_| |_/\_| \_| \____/\_| |_/"
 	print ''
 	print ''
 	time.sleep(0.3)
-	lol = raw_input(""+T+"                                 Website Name :> " + color.END)
-	print(""+M+"          Below are the list of websites with info on " +lol+ "" +color.END)
+	lol = raw_input(""+T+color.BOLD+"                                      QUERY :> " + color.END)
+	print(""+M+color.BOLD+"                    Below are the list of websites with info on " +lol+ "" +color.END)
 	for url in search(lol, tld='com', lang='es', stop=50):
-		print(""+O+"               Info Sites Found :> "+W+"" + url)
+		print(""+O+color.BOLD+"                    Info Sites Found :> "+W+"" + url)
