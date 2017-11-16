fb10befe060d2a42c2e1b4ebebd33dd0daaa1840
the-Infected-Drake <robinsonpinaxx2000@gmail.com>
2017-11-16 15:52:46 +0530

    Update geoIP.py

--- tidos/geoIP.py
+++ tidos/geoIP.py
@@ -33,11 +33,11 @@ def geoIP():
     time.sleep(0.1)
     print color.CYAN+color.BOLD+"                              |_______||_____|_____||_______|___|   "                   
     print ""                                                                  
-    print ""+GR+color.BOLD+"                      +======================================================+"
+    print ""+B+color.BOLD+"                      +======================================================+"
     time.sleep(0.1)
-    print(''+B+color.BOLD+'                             Enter website address for the GeoIP LookUP')
+    print(''+GR+color.BOLD+'                             Enter website address for the GeoIP LookUP')
     time.sleep(0.1)
-    print(''+GR+color.BOLD+'                      +======================================================+')
+    print(''+B+color.BOLD+'                      +======================================================+')
     h = raw_input(''+ T + color.BOLD + '                                 Website :> ' + color.END)
     time.sleep(0.4)
     print('' + GR + color.BOLD + '                                LookUP begins...')
